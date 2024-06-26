453e17e97062399d86f3f60c6a0a241720a289da
Daniel <danielalday4@gmail.com>
2024-06-26 10:56:26 -0400

    build: form created

--- frontend/src/creacion-evento/creacion-evento.component.ts
+++ frontend/src/creacion-evento/creacion-evento.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-creacion-evento',
   standalone: true,
   imports: [],
   templateUrl: './creacion-evento.component.html',
-  styleUrl: './creacion-evento.component.css'
+  styleUrl: './creacion-evento.component.css',
 })
 export class CreacionEventoComponent {
+  //Variables del controlador web
+  datos_solicitante: FormGroup = new FormGroup({});
+  datos_evento: FormGroup = new FormGroup({});
 
+  ngOnInit() {
+    this.datos_solicitante = new FormGroup({
+      nombre: new FormControl<string>(''),
+      apellido: new FormControl<string>(''),
+      correo: new FormControl<string>(''),
+    });
+
+    this.datos_evento = new FormGroup({
+      nombre_evento: new FormControl<string>(''),
+      fecha_inicio: new FormControl<string>(''),
+      fecha_termino: new FormControl<string>(''),
+      hora_inicio: new FormControl<string>(''),
+      hora_termino: new FormControl<string>(''),
+      lugar_evento: new FormControl<string>(''),
+      tipo_evento: new FormControl<string>(''),
+    });
+  }
+  comprobarCrearSolicitud() {
+    if (this.datos_evento.invalid && this.datos_solicitante.invalid) {
+      //esta invalidi el formulario
+    }
+  }
 }
