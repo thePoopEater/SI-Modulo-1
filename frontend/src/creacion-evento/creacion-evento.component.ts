df87831fd5e9769ee7336d7749db8b11f9ab788c
chopancho <pancho.pantoja@outlook.es>
2024-06-26 10:37:26 -0400

    wip: vista

---
+++ frontend/src/creacion-evento/creacion-evento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creacion-evento',
+  standalone: true,
+  imports: [],
+  templateUrl: './creacion-evento.component.html',
+  styleUrl: './creacion-evento.component.css'
+})
+export class CreacionEventoComponent {
+
+}
