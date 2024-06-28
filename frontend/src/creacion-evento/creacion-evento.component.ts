65f128e5555acb191fbc3576f50ab9ff9b3ba0a4
Daniel <danielalday4@gmail.com>
2024-06-28 19:17:28 -0400

    commit tomas silva

--- frontend/src/creacion-evento/creacion-evento.component.ts
+++ frontend/src/creacion-evento/creacion-evento.component.ts
@@ -1,12 +1,20 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, Time } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Servicio } from '../model/Servicio';
 import { Validator } from '@angular/forms';
+import { Solicitante } from '../model/Solicitante';
+import { SolicitudEvento } from '../model/SolicitudEvento';
 @Component({
   selector: 'app-creacion-evento',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './creacion-evento.component.html',
   styleUrl: './creacion-evento.component.css',
 })
@@ -37,17 +45,46 @@ export class CreacionEventoComponent {
       nombre_evento: new FormControl<string>('', [Validators.required]),
       fecha_inicio: new FormControl<string>('', [Validators.required]),
       fecha_termino: new FormControl<string>('', [Validators.required]),
-      hora_inicio: new FormControl<string>('', [Validators.required]),
-      hora_termino: new FormControl<string>('', [Validators.required]),
+      hora_inicio: new FormControl<Time>({ hours: 0, minutes: 0 }, [
+        Validators.required,
+      ]),
+      hora_termino: new FormControl<Time>({ hours: 0, minutes: 0 }, [
+        Validators.required,
+      ]),
       lugar_evento: new FormControl<string>('', [Validators.required]),
       tipo_evento: new FormControl<string>('', [Validators.required]),
     });
   }
 
   comprobarCrearSolicitud() {
-    if (this.datos_evento.valid && this.datos_solicitante.valid) {
+    if (this.datos_solicitante.valid) {
       console.log('Es valido');
+      // Campos de datos solicitante
+      const nombre = this.datos_solicitante.controls['nombre'].value;
+      const apellido = this.datos_solicitante.controls['apellido'].value;
+      const correo = this.datos_solicitante.controls['correo'].value;
+      // Campos de solicitud evento
+      const nombre_evento = this.datos_evento.controls['nombre_evento'].value;
+      const fecha_inicio = this.datos_evento.controls['fecha_inicio'].value;
+      const fecha_termino = this.datos_evento.controls['fecha_termino'].value;
+      const hora_inicio = this.datos_evento.controls['hora_inicio'].value;
+      const hora_termino = this.datos_evento.controls['hora_termino'].value;
+      const lugar_evento = this.datos_evento.controls['lugar_evento'].value;
+      const tipo_evento = this.datos_evento.controls['tipo_evento'].value;
+
+      const datos_solicitante = new Solicitante(nombre, apellido, correo);
+      const solicitud_evento = new SolicitudEvento(
+        nombre_evento,
+        fecha_inicio,
+        fecha_termino,
+        hora_inicio,
+        hora_termino,
+        lugar_evento,
+        tipo_evento
+      );
+      alert('Esta bueno');
+    } else {
+      alert('Esta malo');
     }
-    console.log('Es invalido');
   }
 }
--- frontend/src/model/SolicitudEvento.ts
+++ frontend/src/model/SolicitudEvento.ts
@@ -6,18 +6,24 @@ export class SolicitudEvento {
   fecha_termino: Date = new Date();
   hora_inicio: Time = { hours: 0, minutes: 0 };
   hora_termino: Time = { hours: 0, minutes: 0 };
+  lugar_evento: string = '';
+  tipo_evento: string = '';
 
   constructor(
     nombre_evento: string,
     f_inicio: Date,
     f_termino: Date,
     h_inicio: Time,
-    h_termino: Time
+    h_termino: Time,
+    lugar_evento: string,
+    tipo_evento: string
   ) {
     this.nombre_evento = nombre_evento;
     this.fecha_inicio = f_inicio;
     this.fecha_termino = f_termino;
     this.hora_inicio = h_inicio;
     this.hora_termino = h_termino;
+    this.lugar_evento = lugar_evento;
+    this.tipo_evento = tipo_evento;
   }
 }
